a4df2841cef23a5fd4ff6e70b9c0f665bed3096e
Daniel Bell <daniel@Daniels-MBP-2.fios-router.home>
2018-12-30 22:26:55 -0500

    Fixed newsletter headline

--- morty-assignment/src/app/newsletter/newsletter.component.ts
+++ morty-assignment/src/app/newsletter/newsletter.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newsletter.component.scss']
 })
 export class NewsletterComponent implements OnInit {
-  headline:string="Stay in Touch";
+  headline:string="Create your Holiday";
   newsletter_headline:string="Hi! What are you holiday interests?";
   cta_title:string="Search Partners";
   constructor() { }
